80d92327fab3e31b6e3c7ef68e33efa83ae667f7
bunnie <bunnie@kosagi.com>
2018-08-21 18:02:02 +0800

    update to match the latest changes to litedram, litex

--- netv2mvp.py
+++ netv2mvp.py
@@ -475,9 +475,7 @@ class BaseSoC(SoCSDRAM):
         # sdram
         iodelay_clk_freq = int(400e6)
         self.submodules.ddrphy = a7ddrphy.A7DDRPHY(platform.request("ddram"), iodelay_clk_freq=iodelay_clk_freq)
-        self.ddrphy.settings.rtt_nom = '40ohm'
-        self.ddrphy.settings.drive_strength = '40ohm'
-        self.ddrphy.settings.dynamic_odt = '60ohm'
+        self.ddrphy.settings.add_electrical_settings(rtt_nom='40ohm', rtt_wr='60ohm', ron='40ohm') # TODO revisit for 100T
         self.add_constant("IDELAYCTRL_CLOCK_FREQUENCY", int(iodelay_clk_freq))
         sdram_module = K4B2G1646FBCK0(self.clk_freq, "1:4", speedgrade='1600')
         self.add_constant("READ_LEVELING_BITSLIP", 3)

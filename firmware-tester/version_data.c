4b8072ca26375257264e96f04fb7fae09fa24b82
bunnie <bunnie@kosagi.com>
2018-10-14 00:07:51 +0800

    update the tester images after fixing tester generation script

--- firmware-tester/version_data.c
+++ firmware-tester/version_data.c
@@ -2,14 +2,17 @@
 const char* board = "NeTV2";
 const char* target = "Test";
 
-const char* git_commit = "5365cc456f2bf32ab2de0ba6c78516109f40d803";
+const char* git_commit = "5536f80c030ec0959914d8bd2775fef301a8041c";
 const char* git_branch = "tester-master";
-const char* git_describe = "v0.0-29-g5365cc4-dirty";
+const char* git_describe = "v0.0-39-g5536f80-dirty";
 const char* git_status =
     "    --\r\n"
-   "     M sdcard.c\r\n"
-   "     M tester.c\r\n"
    "     M version_data.c\r\n"
+   "     M ../tester-images/gtptester-100.bit\r\n"
+   "     M ../tester-images/gtptester-35.bit\r\n"
+   "     M ../tester-images/gtptester-firmware.bin\r\n"
+   "     M ../tester-images/tester-100.bit\r\n"
+   "     M ../tester-images/tester-35.bit\r\n"
    "    ?? ../.idea/\r\n"
    "    ?? ../__pycache__/\r\n"
    "    ?? ../deps/site/__pycache__/\r\n"

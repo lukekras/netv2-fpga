49266064b2d0798db1930ffd5f6b89766548e8f3
bunnie <bunnie@kosagi.com>
2018-10-26 15:46:47 +0800

    update images with latest changes

--- firmware-tester/version_data.c
+++ firmware-tester/version_data.c
@@ -2,14 +2,17 @@
 const char* board = "NeTV2";
 const char* target = "Test";
 
-const char* git_commit = "c57635bb547c21dd8a82eb468bd2df49635517af";
+const char* git_commit = "77672eeadd50b9e3f2c446bffa122bb4b7349644";
 const char* git_branch = "tester-master";
-const char* git_describe = "v0.0-55-gc57635b-dirty";
+const char* git_describe = "v0.0-56-g77672ee-dirty";
 const char* git_status =
     "    --\r\n"
-   "     M sdcard.c\r\n"
-   "     M tester.c\r\n"
    "     M version_data.c\r\n"
+   "     M ../tester-images/gtptester-100.bit\r\n"
+   "     M ../tester-images/gtptester-35.bit\r\n"
+   "     M ../tester-images/gtptester-firmware.bin\r\n"
+   "     M ../tester-images/tester-100.bit\r\n"
+   "     M ../tester-images/tester-35.bit\r\n"
    "    ?? ../.idea/\r\n"
    "    ?? ../__pycache__/\r\n"
    "    ?? ../deps/site/__pycache__/\r\n"

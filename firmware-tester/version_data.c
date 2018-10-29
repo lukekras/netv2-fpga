9af66d557321a9f4a98d31d83400dff4f20a58b7
bunnie <bunnie@kosagi.com>
2018-10-30 01:27:18 +0800

    update production tester firmware to the latest changes

--- firmware-tester/version_data.c
+++ firmware-tester/version_data.c
@@ -2,13 +2,17 @@
 const char* board = "NeTV2";
 const char* target = "Test";
 
-const char* git_commit = "b80285795433047b046d966417187cf2f7d05900";
+const char* git_commit = "4b25ee26adbe95929a51be45c7bbeabaf5b3f4a2";
 const char* git_branch = "tester-master";
-const char* git_describe = "v0.0-59-gb802857-dirty";
+const char* git_describe = "v0.0-60-g4b25ee2-dirty";
 const char* git_status =
     "    --\r\n"
-   "     M tester.c\r\n"
    "     M version_data.c\r\n"
+   "     M ../tester-images/gtptester-100.bit\r\n"
+   "     M ../tester-images/gtptester-35.bit\r\n"
+   "     M ../tester-images/gtptester-firmware.bin\r\n"
+   "     M ../tester-images/tester-100.bit\r\n"
+   "     M ../tester-images/tester-35.bit\r\n"
    "    ?? ../.idea/\r\n"
    "    ?? ../__pycache__/\r\n"
    "    ?? ../deps/site/__pycache__/\r\n"

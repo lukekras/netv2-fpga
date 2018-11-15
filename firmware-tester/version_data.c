6f421022afab5397c5212c6533868c65a293af41
bunnie <bunnie@kosagi.com>
2018-11-15 23:20:47 +0800

    tracing down weird firmware build problem

--- firmware-tester/version_data.c
+++ firmware-tester/version_data.c
@@ -2,13 +2,11 @@
 const char* board = "NeTV2";
 const char* target = "Test";
 
-const char* git_commit = "da9777844bf2134692a0184d4a6428cb048e2016";
+const char* git_commit = "fa9784a367cb5fbdd202ddd8b5d694a2f7b6c538";
 const char* git_branch = "tester-master";
-const char* git_describe = "v1.0.1-3-gda97778-dirty";
+const char* git_describe = "v1.0.1-4-gfa9784a";
 const char* git_status =
     "    --\r\n"
-   "     M ../netv2mvp.py\r\n"
-   "     M ../tester-images/tester-35.bit\r\n"
    "    ?? ../.idea/\r\n"
    "    ?? ../__pycache__/\r\n"
    "    ?? ../deps/site/__pycache__/\r\n"

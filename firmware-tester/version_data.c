da9777844bf2134692a0184d4a6428cb048e2016
bunnie <bunnie@kosagi.com>
2018-11-15 21:14:19 +0800

    update bios fix

--- firmware-tester/version_data.c
+++ firmware-tester/version_data.c
@@ -2,13 +2,17 @@
 const char* board = "NeTV2";
 const char* target = "Test";
 
-const char* git_commit = "1d37536ecda2890fa49b685de0d3c41164872897";
+const char* git_commit = "08e4fc438ca9c80700b450a20032e0d00868e835";
 const char* git_branch = "tester-master";
-const char* git_describe = "v1.0.1-dirty";
+const char* git_describe = "v1.0.1-2-g08e4fc4-dirty";
 const char* git_status =
     "    --\r\n"
-   "     M ../deps/litex\r\n"
+   "     M version_data.c\r\n"
+   "     M ../tester-images/gtptester-100.bit\r\n"
    "     M ../tester-images/gtptester-35.bit\r\n"
+   "     M ../tester-images/gtptester-firmware.bin\r\n"
+   "     M ../tester-images/tester-100.bit\r\n"
+   "     M ../tester-images/tester-35.bit\r\n"
    "    ?? ../.idea/\r\n"
    "    ?? ../__pycache__/\r\n"
    "    ?? ../deps/site/__pycache__/\r\n"

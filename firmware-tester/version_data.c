8ab53e2dee36b4945281716eca5d2d9834999c6d
bunnie <bunnie@kosagi.com>
2018-11-16 22:12:03 +0800

    push version for ddr600 fix testing

--- firmware-tester/version_data.c
+++ firmware-tester/version_data.c
@@ -2,15 +2,17 @@
 const char* board = "NeTV2";
 const char* target = "Test";
 
-const char* git_commit = "0046aafd26ba6c058261e9cef3775bb2cb68a0fe";
+const char* git_commit = "77897df3c8457fea55e59581e9fb6be2d745f34f";
 const char* git_branch = "tester-master";
-const char* git_describe = "v1.0.4-dirty";
+const char* git_describe = "v1.0.4-1-g77897df-dirty";
 const char* git_status =
     "    --\r\n"
    "     M ../deps/litex\r\n"
-   "     M hdmi_in0.c\r\n"
-   "     M hdmi_in1.c\r\n"
-   "     M ../netv2mvp.py\r\n"
+   "     M version_data.c\r\n"
+   "     M ../tester-images/gtptester-100.bit\r\n"
+   "     M ../tester-images/gtptester-35.bit\r\n"
+   "     M ../tester-images/gtptester-firmware.bin\r\n"
+   "     M ../tester-images/tester-100.bit\r\n"
    "     M ../tester-images/tester-35.bit\r\n"
    "    ?? ../.idea/\r\n"
    "    ?? ../__pycache__/\r\n"
